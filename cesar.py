d62be31a5bbbe57cefc3015df87770ba5cae13d3
rkorzen <korzeniewski@gmail.com>
2022-04-23 14:51:28 +0200

    Tresc zadania

---
+++ cesar.py
@@ -0,0 +1,25 @@
+"""
+Napisz funkcję encrypt, która zaszyfruje tekst zgodnie z szyfrem cesara.
+Oraz decrypt, która odkoduje ten tekst.
+
+    >>> encrypt("CEASER CIPHER DEMO", 4)
+    IEWIVrGMTLIVrHIQS
+    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'CEASERzCIPHERzDEMO'
+
+Wersj trudniejsza
+
+    >>> decrypt("IEWIVrGMTLIVrHIQS", 4)
+    'CEASER CIPHER DEMO'
+
+
+przesuniecie 1
+"ABCD"
+"BCDA"
+
+
+przesuniecie 2
+"ABCD"
+"CDAB"
+
+"""
