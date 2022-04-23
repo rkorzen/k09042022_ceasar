baba26b8e27bff280bbdb9429f5d97daa8c52a0c
rkorzen <korzeniewski@gmail.com>
2022-04-23 15:16:42 +0200

    decrypt function

--- cesar.py
+++ cesar.py
@@ -35,8 +35,21 @@ def encrypt(text: str, shift: int) -> str:
     return result
 
 
+def decrypt(text: str, shift: int) -> str:
+    result = ""
+    space = chr((ord(" ") + shift - 97) % 26 + 97)
+    for char in text:
+        if char == space:
+            result += " "
+        elif char.isupper():
+            result += chr((ord(char) - shift - 65) % 26 + 65)
+        else:
+            result += chr((ord(char) - shift - 97) % 26 + 97)
+    return result
+
+
 if __name__ == "__main__":
     assert encrypt("CEASER CIPHER DEMO", 4) == "GIEWIVrGMTLIVrHIQS"
     assert encrypt("A", 1) == "B"
-    assert decrypt("GIEWIVrGMTLIVrHIQS", 4) == "CEASERzCIPHERzDEMO"
+    assert decrypt("GIEWIVrGMTLIVrHIQS", 4) == "CEASER CIPHER DEMO"
     assert decrypt("B", 1) == "A"
